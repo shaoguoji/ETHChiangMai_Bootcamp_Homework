5f98d798b89c500f7ed14f98e894636ba68e5656
shaoguoji <752147916@qq.com>
2025-12-12 16:54:30 +0700

    [Update]homework.

--- Day4/BigBank.sol
+++ Day4/BigBank.sol
@@ -25,7 +25,7 @@ contract Bank is IBank {
         userBlance[user] += amount;
     }
 
-    function withDraw(uint256 value) external  override virtual {
+    function withDraw(uint256 value) external override virtual {
         if ((msg.sender == owner) && (address(this).balance >= value)) {
             payable(msg.sender).transfer(value);
         } 
@@ -64,13 +64,18 @@ contract Bank is IBank {
 }
 
 contract BigBank is Bank {
-    function deposit(address user, uint256 amount) public override {
+    modifier checkAmount(uint256 amount) {
         require(amount > 0.001 ether, "Deposit value must be greater than 0.001!");
-        userBlance[user] += amount;
+        _;
+    }
+
+    function deposit(address user, uint256 amount) public override checkAmount(amount){
+        super.deposit(user, amount);
     }
 
     function transferOwner(address newOwner) public{
         require(msg.sender == owner, "Only owner be allow to do this!");
+        require(newOwner == address(0), "Invalid input address.");
         owner = newOwner;
     }
 }
@@ -82,9 +87,7 @@ contract Admin {
         owner = msg.sender;
     }
 
-    receive() external payable { 
-        
-    }
+    receive() external payable { }
 
     function adminWithdraw(IBank bank) public {
         require(owner == msg.sender, "Only owner be allow to do this!");
