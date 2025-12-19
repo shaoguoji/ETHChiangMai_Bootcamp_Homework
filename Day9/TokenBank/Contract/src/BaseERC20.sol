a3edfb1ef12733c13f95c3170911d9a94b965c70
shaoguoji <752147916@qq.com>
2025-12-19 17:59:26 +0700

    [Add]contract files.

---
+++ Day9/TokenBank/Contract/src/BaseERC20.sol
@@ -0,0 +1,107 @@
+// https://decert.me/challenge/aa45f136-27a3-4bc9-b4f7-15308e1e0daa
+
+/**
+题目#1
+编写 ERC20 token 合约
+  
+介绍
+ERC20 是以太坊区块链上最常用的 Token 合约标准。通过这个挑战，你不仅可以熟悉 Solidity 编程，而且可以了解 ERC20 Token 合约的工作原理。
+
+目标
+完善合约，实现以下功能：
+
+设置 Token 名称（name）："BaseERC20"
+设置 Token 符号（symbol）："BERC20"
+设置 Token 小数位decimals：18
+设置 Token 总量（totalSupply）:100,000,000
+允许任何人查看任何地址的 Token 余额（balanceOf）
+允许 Token 的所有者将他们的 Token 发送给任何人（transfer）；转帐超出余额时抛出异常(require),并显示错误消息 “ERC20: transfer amount exceeds balance”。
+允许 Token 的所有者批准某个地址消费他们的一部分Token（approve）
+允许任何人查看一个地址可以从其它账户中转账的代币数量（allowance）
+允许被授权的地址消费他们被授权的 Token 数量（transferFrom）；
+转帐超出余额时抛出异常(require)，异常信息：“ERC20: transfer amount exceeds balance”
+转帐超出授权数量时抛出异常(require)，异常消息：“ERC20: transfer amount exceeds allowance”。
+注意：
+在编写合约时，需要遵循 ERC20 标准，此外也需要考虑到安全性，确保转账和授权功能在任
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract BaseERC20 {
+    string public name; 
+    string public symbol; 
+    uint8 public decimals; 
+
+    uint256 public totalSupply; 
+
+    mapping (address => uint256) balances; 
+
+    mapping (address => mapping (address => uint256)) allowances; 
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor() {
+        // write your code here
+        // set name,symbol,decimals,totalSupply
+        name = "BaseERC20";
+        symbol = "BERC20";
+        decimals = 18;
+        totalSupply = 1e8*1e18;
+
+        balances[msg.sender] = totalSupply;  
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        // write your code here
+        require(_owner != address(0), "zero adress");
+        return balances[_owner];
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        // write your code here
+        require(_to != address(0), "zero address");
+        require(balances[msg.sender] > 0, "no money");
+        require(balances[msg.sender] >= _value, "ERC20: transfer amount exceeds balance");
+
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);  
+        return true;   
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        // write your code here
+        require(_from != address(0));
+        require(_to != address(0));
+        require(balances[_from] >= _value, "ERC20: transfer amount exceeds balance");
+        require(allowances[_from][msg.sender] >= _value, "ERC20: transfer amount exceeds allowance");
+
+        allowances[_from][msg.sender] -= _value;
+        balances[_from] -= _value;
+        balances[_to] += _value;
+
+        emit Transfer(_from, _to, _value); 
+        return true; 
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        // write your code here
+        require(_spender != address(0), "zero address");
+
+        allowances[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value); 
+        return true; 
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {   
+        // write your code here
+        require(_owner != address(0));
+        require(_spender != address(0));
+        
+        return allowances[_owner][_spender];
+    }
+}
